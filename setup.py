74a44fc50c98d49974f31072d5b16f3afe74a310
Alexander Chekunkov <chekunkov@gmail.com>
2014-11-05 16:47:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="scrapy-splash",
+    version='0.0.1',
+    author='Paul Tremberth',
+    author_email='info@scrapinghub.com',
+    maintainer='Scrapinghub',
+    maintainer_email='info@scrapinghub.com',
+    py_modules=['scrapy_splash'],
+    install_requires=['Scrapy'],
+)
